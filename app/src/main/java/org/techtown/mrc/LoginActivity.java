c8ec4bc00a22c0ab0e25943f5223aee51de2e229
Seongwoo-h <hsw9420@naver.com>
2022-05-28 21:35:32 +0900

    //로그인, 회원가입

--- app/src/main/java/org/techtown/mrc/LoginActivity.java
+++ app/src/main/java/org/techtown/mrc/LoginActivity.java
@@ -5,9 +5,12 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,38 +22,91 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import org.techtown.mrc.databinding.ActivityLoginBinding;
+import org.techtown.mrc.utilities.Constants;
+import org.techtown.mrc.utilities.PreferenceManager;
+
+import java.util.regex.Pattern;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private EditText et_user;
-    private Button btn_login;
+    private ActivityLoginBinding binding;
+    private PreferenceManager preferenceManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-        setTitle("로그인");
-        setContentView(R.layout.activity_login);
+        preferenceManager = new PreferenceManager(getApplicationContext());
+        if(preferenceManager.getBoolean(Constants.KEY_IS_SIGNED_IN)){
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
+    }
 
-        et_user = (EditText) findViewById(R.id.et_user);
-        btn_login = (Button) findViewById(R.id.btn_login);
-
-        btn_login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String str_name = et_user.getText().toString();
-
-                if("".equals(str_name)){
-                    Toast.makeText(LoginActivity.this,"닉네임을 입력해주세요!",Toast.LENGTH_SHORT).show();
-                }
-                else{
-                    Intent intent = new Intent(LoginActivity.this,MainActivity.class);
-                    intent.putExtra("name",str_name);
-                    startActivity(intent);
-                }
+    private void setListeners(){
+        binding.textCreateNewAccount.setOnClickListener(v ->
+                startActivity(new Intent(getApplicationContext(),SignUpActivity.class)));
+        binding.buttonSignIn.setOnClickListener(v -> {
+            if(isValidLoginDetails()){
+                Login();
             }
         });
+    }
 
+    private void Login(){
+        loading(true);
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        database.collection(Constants.KEY_COLLECTION_USERS)
+                .whereEqualTo(Constants.KEY_EMAIL, binding.inputEmail.getText().toString())
+                .whereEqualTo(Constants.KEY_PASSWORD, binding.inputPassword.getText().toString())
+                .get()
+                .addOnCompleteListener(task -> {
+                    if(task.isSuccessful() && task.getResult()!=null
+                        && task.getResult().getDocuments().size()>0){
+                        DocumentSnapshot documentSnapshot = task.getResult().getDocuments().get(0);
+                        preferenceManager.putBoolean(Constants.KEY_IS_SIGNED_IN,true);
+                        preferenceManager.putString(Constants.KEY_USER_ID, documentSnapshot.getId());
+                        preferenceManager.putString(Constants.KEY_NAME, documentSnapshot.getString(Constants.KEY_NAME));
+                        preferenceManager.putString(Constants.KEY_IMAGE, documentSnapshot.getString(Constants.KEY_IMAGE));
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(intent);
+                    }else{
+                        loading(false);
+                        showToast("로그인 할 수 없음");
+                    }
+                });
+    }
 
+    private void loading(Boolean isLoading){
+        if(isLoading) {
+            binding.buttonSignIn.setVisibility(View.INVISIBLE);
+            binding.progressBar.setVisibility(View.VISIBLE);
+        }else{
+            binding.progressBar.setVisibility(View.INVISIBLE);
+            binding.buttonSignIn.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void showToast(String message){
+        Toast.makeText(getApplicationContext(),message,Toast.LENGTH_SHORT).show();
+    }
+
+    private Boolean isValidLoginDetails(){
+        if(binding.inputEmail.getText().toString().trim().isEmpty()) {
+            showToast("이메일 입력을 입력하세요");
+            return false;
+        }else if(!Patterns.EMAIL_ADDRESS.matcher(binding.inputEmail.getText().toString()).matches()){
+            showToast("이메일의 형식이 올바르지 않습니다");
+            return false;
+        }else if(binding.inputPassword.getText().toString().trim().isEmpty()){
+            showToast("비밀번호를 입력하세요");
+            return false;
+        }else{
+            return true;
+        }
     }
 }
