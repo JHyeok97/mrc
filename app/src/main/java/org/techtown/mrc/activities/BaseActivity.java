109f3759b894a17a0ca96e67b91474d6496280f0
Seongwoo-h <hsw9420@naver.com>
2022-09-29 21:20:09 +0900

    //로그인, 회원가입 //친구추가, 로그아웃, 목록 등등 //

--- app/src/main/java/org/techtown/mrc/activities/BaseActivity.java
+++ app/src/main/java/org/techtown/mrc/activities/BaseActivity.java
@@ -5,10 +5,16 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import org.techtown.mrc.utilities.PreferenceManager;
+
 public class BaseActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        PreferenceManager preferenceManager = new PreferenceManager(getApplicationContext());
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
     }
 }
